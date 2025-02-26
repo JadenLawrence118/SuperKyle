9bc950775145ead904907ced133f0e1d239c7082
Jaden Lawrence <jaden2.lawrence@live.uwe.ac.uk>
2025-02-26 00:25:52 +0000

    Reupload
    Reupload of this project in the state it was last worked on

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] public Transform player;
+    private float camX = 0;
+    private float camY = 0;
+
+    public bool movingCam = true;
+
+    private void Update()
+    {
+        if (movingCam == true)
+        {
+            if (player.position.y >= -10.73)
+            {
+                camY = player.position.y;
+            }
+            else
+            {
+                camY = transform.position.y;
+            }
+
+            if (player.position.x >= 0)
+            {
+                camX = player.position.x;
+            }
+            else
+            {
+                camX = transform.position.x;
+            }
+
+            transform.position = new Vector3(camX, camY, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D solidObj;
+
+    public CameraControl cam;
+
+    private void Start()
+    {
+        solidObj = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        cam.movingCam = false;
+        anim.SetTrigger("death");
+        solidObj.bodyType = RigidbodyType2D.Static;
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
